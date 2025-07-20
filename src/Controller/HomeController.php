5abc86e6b8e09eeb2511b1c37c7f5fdd9f760e60
kibaiz881 <solofo881@gmail.com>
2025-07-20 19:52:34 +0200

    create first page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $slug = 'Home';
+        return $this->render('home/index.html.twig', [
+            'slug' => $slug,
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
